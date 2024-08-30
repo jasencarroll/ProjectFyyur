6870502428451315249bde4f33fcaca1c0a1cf3d
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-30 06:56:05 -0400

    started updated models

--- app.py
+++ app.py
@@ -39,6 +39,12 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    image_link = db.Column(db.String)
+    past_shows = db.Column(db.String)
+    upcoming_shows = db.Column(db.String)
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -53,10 +59,27 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    image_link = db.Column(db.String)
+    past_shows = db.Column(db.String)
+    upcoming_shows = db.Column(db.String)
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+    __tablename__ = "Show"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    artist_id = db.Column(db.Integer, foreign_key=True)
+    artist_name = db.Column(db.String)
+    artist_image_link = db.Column(db.String)
+    start_time =  db.Column(db.String)
+
 
 #----------------------------------------------------------------------------#
 # Filters.

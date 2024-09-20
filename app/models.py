bc96ad39cc043f9b358af69a9b1f6496ca67a8f1
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-09-20 16:08:41 -0400

    completed models

--- app/models.py
+++ app/models.py
@@ -15,12 +15,15 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    image_link = db.Column(db.String)
-    # relationship to shows
-
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.String(120))  # Add genres field
+    website_link = db.Column(db.String(120))  # Add website_link field
+    seeking_talent = db.Column(db.Boolean, default=False) 
+    seeking_description = db.Column(db.String(500))
+    # Define the relationship with the Show model
+    shows = db.relationship('Show', backref='venue', lazy=True)
+    
+    def __repr__(self): 
+        return f'<Venue {self.id} {self.name}>' 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -33,24 +36,24 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    image_link = db.Column(db.String)
-    # relationship to shows
+    website_link = db.Column(db.String(120)) 
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True) 
 
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
 
 class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    # get artist name
-    # get artist image
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    # get venue name
-    # get venue image
-    start_time =  db.Column(db.String)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+
+    artist = db.relationship('Artist', backref=db.backref('shows', cascade='all, delete'))
+    venue = db.relationship('Venue', backref=db.backref('shows', cascade='all, delete'))
+
+    def __repr__(self):
+        return f'<Show {self.id} {self.artist_id} {self.venue_id} {self.start_time}>'
